d4c97bb21f0d6a33f06ae4cb275553676886b088
ranim240 <khairallahranim@gmail.com>
2026-04-05 19:48:53 -0400

    task3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
 
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
     # path for dealer reviews view
 
 
