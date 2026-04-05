cd61618c7a221fce9b80deebaf7698a6449d0243
ranim240 <khairallahranim@gmail.com>
2026-04-05 16:54:43 -0400

    signup, login, and logout

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Register/Register"
+import RegisterPanel from "./components/Login/Login"
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel />} />
+
     </Routes>
   );
 }
