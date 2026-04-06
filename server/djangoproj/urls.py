fce721a150ac32245115def14a3f671021a76de5
ranim240 <khairallahranim@gmail.com>
2026-04-06 05:37:46 -0400

    _task

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -23,8 +23,12 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
 
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
